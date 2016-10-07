f1f231fec163726fc7c05579caade8aea4281e79
chrislpatton <chrislpatton@gmail.com>
2016-10-07 18:22:15 -0400

    fixed error

--- savedOrders.js
+++ savedOrders.js
@@ -4,8 +4,8 @@
 
 var recovered = [];
 for (var i = 0; i < lostOrders.length; i++) {
-  for (var j= 0; j < savedOrders.length; j++) {
-    if(savedOrders.order_id[j] === lostOrders[i]) {
+  for (var j = 0; j < savedOrders.length; j++) {
+    if(savedOrders[j].order_id === lostOrders[i]) {
       recovered.push(savedOrders[j]);
     }
   }
